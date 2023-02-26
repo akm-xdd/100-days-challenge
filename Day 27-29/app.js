56753bcfdb62e46b718a6f08741f9697f27a0d5c
Anand Kumar Mishra <110248822+akm-xdd@users.noreply.github.com>
2023-02-26 19:44:39 +0530

    Day 27-29 started

---
+++ Day 27-29/app.js
@@ -0,0 +1,14 @@
+// let age = 19;
+// let name = "John";
+// let isMale = true;
+// let job;
+// let hobbies = ["Cricket", "Video Games", "Movies"];
+
+let info = {
+  age: 19,
+  name: "John",
+  isMale: true,
+  hobbies: ["Cricket", "Video Games", "Movies"],
+};
+
+alert(JSON.stringify(info));
