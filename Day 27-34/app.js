d57be8da6cf2a23e1bc28dbf97497e7c252475aa
Anand Kumar Mishra <110248822+akm-xdd@users.noreply.github.com>
2023-06-06 12:43:53 +0530

    updated

--- Day 27-34/app.js
+++ Day 27-34/app.js
@@ -4,11 +4,16 @@
 // let job;
 // let hobbies = ["Cricket", "Video Games", "Movies"];
 
-let info = {
-  age: 19,
-  name: "John",
-  isMale: true,
-  hobbies: ["Cricket", "Video Games", "Movies"],
-};
-console.dir(document)
-alert(JSON.stringify(info));
+// let info = {
+//   age: 19,
+//   name: "John",
+//   isMale: true,
+//   hobbies: ["Cricket", "Video Games", "Movies"],
+// };
+// console.dir(document)
+// alert(JSON.stringify(info));
+
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let link = document.getElementById('external-link')
+link.href = 'https://bing.com'
